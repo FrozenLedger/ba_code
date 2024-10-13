f7c76aedb0dad82938d5332220ffbfc4a5659cca
FrozenLedger <denny.falls-rodriguez@stud.hs-bochum.de>
2024-10-13 15:05:19 +0200

    Added command-pattern-implementation.

---
+++ scripts/ba/features/commands/simple_drop_command.py
@@ -0,0 +1,8 @@
+from ba_code.scripts.ba.manipulator.robotarm_adapter import RobotarmAdapter
+
+class SimplePickupCommand:
+    def __init__(self, robotarm) -> None:
+        self.__robotarm: RobotarmAdapter = robotarm
+
+    def execute(self):
+        self.__robotarm.run_drop_instructions()
